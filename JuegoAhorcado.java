532a3082e267f5ff39e60c85d3de03871414752d
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-08 12:55:56 +0100

    Create JuegoAhorcado.java

---
+++ JuegoAhorcado.java
@@ -0,0 +1,15 @@
+package juegoahorcado;
+
+import static juegoahorcado.Ahorcado.primeraParte;
+
+public class JuegoAhorcado {
+
+    public static void main(String[] args) {
+        
+        primeraParte();
+        
+    }
+    
+
+    
+}
