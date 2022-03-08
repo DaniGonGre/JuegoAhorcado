8d012f0e1f194395e873e1dd94069dbb877a3752
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-08 13:26:41 +0100

    Update JuegoAhorcado.java

--- JuegoAhorcado.java
+++ JuegoAhorcado.java
@@ -1,15 +1,26 @@
 package juegoahorcado;
 
-import static juegoahorcado.Ahorcado.primeraParte;
+import javax.swing.JOptionPane;
+import static juegoahorcado.Ahorcado.partida;
 
 public class JuegoAhorcado {
 
     public static void main(String[] args) {
         
-        primeraParte();
+        int op;
+        
+        op = Integer.parseInt(JOptionPane.showInputDialog(" **** MENU ****" +
+            "\n1 --> Empezar partida" + "\n2 --> Sair da partida"));
+        
+        switch (op){
+            case 1 : partida();
+            break;        
+        } while (op != 2);
+            
         
     }
     
 
     
 }
+
