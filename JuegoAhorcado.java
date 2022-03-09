889982f8c50d1a40913fcb3cb8d224fc4d802fae
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-09 13:53:41 +0100

    Update JuegoAhorcado.java

--- JuegoAhorcado.java
+++ JuegoAhorcado.java
@@ -5,22 +5,23 @@ import static juegoahorcado.Ahorcado.partida;
 
 public class JuegoAhorcado {
 
-    public static void main(String[] args) {
-        
+   public static void main(String[] args) {
+
         int op;
-        
+       
+        try{
+
         op = Integer.parseInt(JOptionPane.showInputDialog(" **** MENU ****" +
             "\n1 --> Empezar partida" + "\n2 --> Sair da partida"));
-        
         switch (op){
             case 1 : partida();
             break;        
         } while (op != 2);
-            
+        } catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "El valor ingresado no es un número.");
+        }
         
-    }
-    
-
-    
+        
+    }  
 }
 
