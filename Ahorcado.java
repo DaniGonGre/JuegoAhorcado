4ddfe72ba506593cc7187ed723b4de4c993e1664
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-14 12:47:04 +0100

    Update Ahorcado.java

--- Ahorcado.java
+++ Ahorcado.java
@@ -23,28 +23,32 @@ public class Ahorcado {
         int intentos = 0;
 
         while (true) {
-            if (intentos >= 6) {
-                JOptionPane.showConfirmDialog(null, "Has acabado todos tus intentos.");
+            if (intentos >= 5) {
+                JOptionPane.showMessageDialog(null, "Has acabado todos tus intentos.");
                 break;
             }
-            verPalabras(palabraJuego, jugadores);
             
             if(!jugando(palabraJuego, jugadores)){
                 intentos++;
-                JOptionPane.showMessageDialog(null, "Esta letra no está en la palabra. Te quedan " + (6-intentos) + " intentos");
+                JOptionPane.showMessageDialog(null, "Esta letra no está en la palabra. Te quedan " + (5-intentos) + " intentos");
 
             }
             
             if(verPalabras(palabraJuego, jugadores)){
-                JOptionPane.showMessageDialog(null, "Ganaste");
+               JOptionPane.showMessageDialog(null, "Has acertado la palabra.");
+               break;
             }
+            
+            if (intentos >= 2){
             String tmp = JOptionPane.showInputDialog("Introduce la palabra completa si la sabes: ");
             
+            
             if(palabraJuego.equals(tmp)){
-                JOptionPane.showMessageDialog(null, "Ganaste");
+                JOptionPane.showMessageDialog(null, "Has acertado la palabra.");
                 break;
             } else {
-                JOptionPane.showMessageDialog(null, "No es correcto, continue");
+                JOptionPane.showMessageDialog(null, "La palabra no es correcta.");
+              }
             }
             
         }
@@ -70,7 +74,101 @@ public class Ahorcado {
                 concatena = concatena + " - ";
             }
         }
-        JOptionPane.showConfirmDialog(null, concatena);
+        JOptionPane.showMessageDialog(null, concatena);
         return (palabraJuego.length() == correctas);
+    }
+    
+     public static void partidaDificil() {
+
+        List<String> palabras = new ArrayList<>();
+        Ficherolectura.crearPalabra(palabras);
+        int i = 0;
+
+        Random rand = new Random();
+        String palabraJuego = palabras.get(rand.nextInt(palabras.size()));
+
+        System.out.println(palabraJuego);
+
+        List<Character> jugadores = new ArrayList<>();
+
+        System.out.println();
+
+        int intentos = 0;
+
+        while (true) {
+            if (intentos >= 1) {
+                JOptionPane.showMessageDialog(null, "Has acabado todos tus intentos.");
+                break;
+            }
+            
+            if(!jugando(palabraJuego, jugadores)){
+                intentos++;
+                JOptionPane.showMessageDialog(null, "Esta letra no está en la palabra. Te quedan " + (1-intentos) + " intentos");
+
+            }
+            
+            if(verPalabras(palabraJuego, jugadores)){
+                JOptionPane.showMessageDialog(null, "Ganaste");
+            }
+            
+            if (intentos >= 1){
+            String tmp = JOptionPane.showInputDialog("Introduce la palabra completa si la sabes: ");
+            
+            
+            if(palabraJuego.equals(tmp)){
+                JOptionPane.showMessageDialog(null, "Ganaste");
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "La palabra no es correcta.");
+              }
+            }
+            
+        }
+    }
+     
+      public static void partidaMedia() {
+
+        List<String> palabras = new ArrayList<>();
+        Ficherolectura.crearPalabra(palabras);
+        int i = 0;
+
+        Random rand = new Random();
+        String palabraJuego = palabras.get(rand.nextInt(palabras.size()));
+
+        System.out.println(palabraJuego);
+
+        List<Character> jugadores = new ArrayList<>();
+
+        System.out.println();
+
+        int intentos = 0;
+
+        while (true) {
+            if (intentos >= 3) {
+                JOptionPane.showMessageDialog(null, "Has acabado todos tus intentos.");
+                break;
+            }
+            
+            if(!jugando(palabraJuego, jugadores)){
+                intentos++;
+                JOptionPane.showMessageDialog(null, "Esta letra no está en la palabra. Te quedan " + (3-intentos) + " intentos");
+
+            }
+            
+            if(verPalabras(palabraJuego, jugadores)){
+                JOptionPane.showMessageDialog(null, "Ganaste");
+            }
+            
+            String tmp = JOptionPane.showInputDialog("Introduce la palabra completa si la sabes: ");
+            
+            if(palabraJuego.equals(tmp)){
+                JOptionPane.showMessageDialog(null, "Ganaste");
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "La palabra no es correcta.");
+              }
+            
+            
+        }
     }
 }
