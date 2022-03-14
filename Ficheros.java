e6b977c86929aee2c8007e5d5c470d3c926dca2e
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-03-14 09:47:56 +0100

    Create Ficheros.java

---
+++ Ficheros.java
@@ -0,0 +1,28 @@
+package com.dani.lecturaPalabras;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Fichero {
+
+
+    public static void crearPalabra(List<String> jugadores){
+        File fichero = new File("palabras.txt");
+
+        try{
+            Scanner leer = new Scanner(fichero);
+            while (leer.hasNext()){
+
+                jugadores.add(leer.next());
+            }
+            leer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
